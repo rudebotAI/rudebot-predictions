ca81d7d42b1991d147aa0acc93b135fd983c929d
rudebotAI <rudebot@rudeboyinc.com>
2026-03-27 23:01:09 -0700

    add strategy/kelly.py

---
+++ strategy/kelly.py
@@ -0,0 +1,10 @@
+"""Kelly Criterion Position Sizing"""
+
+class KellyCriterion:
+    def calculate(self, win_rate, av():
+        return 0.25
+
+class ExecuteOrder:
+    def __init__(self, signal):
+        self.signal = signal
+        self.stack = False
